f0184cca28fe751acf174a2f2b32f3c25a7c5468
Administrator <admin@example.com>
2023-10-24 15:51:29 +0800

    hero techstack

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import CallToAction from './common/CallToAction'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <main className='grid grid-cols-2 grid-rows-1 text-white my-16'>
+      <div className='relative flex flex-col justify-center pl-16'>
+        <p className='text-secondary text-md font-light'>Professional Software Developer</p>
+        <p className='text-secondary text-md font-light'>Based in Hong Kong, HK</p>
+        <h1 className='text-5xl pb-8 pt-4'>Hello World<br/> I'm <span className='font-bold text-primary'>Kingson Wong</span></h1>
+        
+        <p className='pb-8 text-md font-base'>
+        “ In the realm of lifelong learning, <br/>
+        there is always something new to glean.”
+        </p>
+        <div className='flex flex-row justify-start gap-4'>
+          <CallToAction text="Download CV" />
+          <a>
+            <img src="/linkdein_icon.png" alt="arrow-down"/>
+          </a>
+        </div>
+        <img className='absolute left-6 bottom-24' src="rect_dec.png" alt="rect"/>
+        <img className='absolute right-6 bottom-24 rotate-45' src="rect_dec.png" alt="rect"/>
+      </div>
+
+      <div className='flex justify-center items-center'>
+        <img src='/my_profile_pic.png' alt="my profile pic"/>
+      </div>
+    </main>
   )
 }
 
--- app/components/TechStackSection.tsx
+++ app/components/TechStackSection.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const TechStackSection = () => {
   return (
-    <div>TechStackSection</div>
+    <section className='flex flex-col justify-center items-center text-white my-16'>
+      <div className='flex flex-col py-8'>
+        <h2 className='text-4xl font-bold text-center'>Tech Stack</h2>
+        <p className='text-center mt-4'>Here are the technologies that I have been working with recently</p>
+      </div>
+      <div className=' flex justify-center'>
+        <img className='w-9/12' src='/techstack.png' alt='techstack'/>
+      </div>
+    </section>
   )
 }
 
